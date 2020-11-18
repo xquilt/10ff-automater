89aa59bbdc202aa7871340701bbf389f438ad81a
mostafa <48662854+z0xyz@users.noreply.github.com>
2020-11-18 12:11:25 +0200

    Update main.js
    well this version does't actually work , but i got sick out of it so far .... so

--- main.js
+++ main.js
@@ -1,14 +1,48 @@
-// 10 FAST FINGERS HACK 
-// phase 1 : retrieve the text 
-// Phase 2 : inputting the retrieved text to the input field 
-// making an event trigger an action of dat input 
-
-function doSomething() {
-	var highlighted  = document.querySelector(".highlight"); 
-	var inputValue  = document.getElementById("inputfield") ; 
-	inputValue.value =  highlighted.textContent ; 
-	inputValue.value = inputValue.value + " "
-}
 
 
-window.addEventListener("keydown" , doSomething , false)
+for  (let i = 0 ; i < itemsCount ; i ++  ) {
+
+        var hWord = document.querySelector(".highlight").textContent ;
+        var hWordLength = hWord.length ; 
+
+        setTimeout(
+
+            () => {
+
+                for (let i = 0; i <= hWordLength ; i ++ ) {
+
+                    setTimeout(
+
+                        () => {
+                              if (i == hWordLength ) {
+                                inputField.value += " "; 
+                                inputField.dispatchEvent(newInput) ;
+
+                              } 
+                                    else if( i < hWordLength) {
+                                    inputField.value += hWord[i] ; 
+                                    inputField.dispatchEvent(newInput) ;
+
+                              } ; 
+
+                            console.log("that's the number " + i ) 
+
+
+                            } , 
+
+                          i * 1000          
+
+                        )  ; 
+
+                    console.log("i should be printed out at the end") ; 
+
+
+                  }
+
+                } , 
+            
+               1000 
+
+         )
+
+    }
