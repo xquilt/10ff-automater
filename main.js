1400ac197c5765699fd2422ac1c3cfdcd664a2f9
mostafa <48662854+z0xyz@users.noreply.github.com>
2021-01-14 18:46:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -13,44 +13,27 @@ for  (let i = 0 ; i < itemsCount ; i ++  ) {
         setTimeout(
 
             () => {
-
                 for (let i = 0; i <= hWordLength ; i ++ ) {
-
                     setTimeout(
-
                         () => {
                               if (i == hWordLength ) {
                                 inputField.value += " "; 
                                 inputField.dispatchEvent(newInput) ;
-
                               } 
                                     else if( i < hWordLength) {
                                     inputField.value += hWord[i] ; 
                                     inputField.dispatchEvent(newInput) ;
-
                               } ; 
-
                             console.log("that's the number " + i ) 
-
-
                             } , 
 
                           i * 1000          
 
                         )  ; 
-
                     console.log("i should be printed out at the end") ; 
-
-
                   }
-
                 } , 
-            
+          
                1000 
-
-
          )
-
-
-
     }
