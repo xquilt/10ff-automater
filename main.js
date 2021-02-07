0e196fe67a4b5e253cce27383bca6881ed082d0f
mostafa <48662854+z0xyz@users.noreply.github.com>
2021-02-07 07:43:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,28 @@
+function sleep(milliseocnds) {
+  const date = Date.now()
+  let currentDate
+  do {
+    currentDate = Date.now()
+  }while((currentDate - date)- milliseocnds)
+}
 
-var inputField = document.getElementsByTagName("input")[0] ; 
-var newInput = new InputEvent("input") ; 
-var itemsCount = document.querySelector(".place").childElementCount ; 
+let inputField = document.getElementsByTagName("input")[0]
+let newInputEvent = new InputEvent("input")
+let wordsCount = document.querySelector(".place").childElementCount
+let promptDelay = window.prompt('add delay between each character') * 1
 
-// this first for loop & the subsequent setTimeout() web api function is causing a problem that i still can't fix ; 
-// so just comment them out later on ... the code so far input each character in every word at a time (which is pretty cool)
-for  (let i = 0 ; i < itemsCount ; i ++  ) {
+function addWord() {  
+  let hightlightedWord = document.querySelector(".highlight").textContent
+  let hWordLength = hightlightedWord.length
 
-        var hWord = document.querySelector(".highlight").textContent ;
-        var hWordLength = hWord.length ; 
+  for (let i = 0; i < hWordLength ; i ++ ) {
+    inputField.value += hightlightedWord[i] ; 
+    inputField.dispatchEvent(newInputEvent) ;
+    sleep(promptDelay)
+  }
+  inputField.value += " "; 
+  inputField.dispatchEvent(newInputEvent) ;
+  inputField.value = ''
+}     
 
-        setTimeout(
-
-            () => {
-                for (let i = 0; i <= hWordLength ; i ++ ) {
-                    setTimeout(
-                        () => {
-                              if (i == hWordLength ) {
-                                inputField.value += " "; 
-                                inputField.dispatchEvent(newInput) ;
-                              } 
-                                    else if( i < hWordLength) {
-                                    inputField.value += hWord[i] ; 
-                                    inputField.dispatchEvent(newInput) ;
-                              } ; 
-                            console.log("that's the number " + i ) 
-                            } , 
-
-                          i * 1000          
-
-                        )  ; 
-                    console.log("i should be printed out at the end") ; 
-                  }
-                } , 
-          
-               1000 
-         )
-    }
+setInterval(addWord , promptDelay)
