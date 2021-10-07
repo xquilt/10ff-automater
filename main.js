40d3e5a12ec4f99082b1a6eca0fc9eb8b7b2d2a7
m <48662854+z0xyz@users.noreply.github.com>
2021-10-07 08:59:22 +0200

    new commit
    New functionality added with few bugs resolved .
    -if the project were to be updated further , then the desired features for now would be appending the ability to add few more wrong kestrokes to add sense of credibility when it comes to error percentage at the final results pane

--- main.js
+++ main.js
@@ -1,28 +1,71 @@
 function sleep(milliseocnds) {
-  const date = Date.now()
-  let currentDate
-  do {
-    currentDate = Date.now()
-  }while((currentDate - date)- milliseocnds)
+    const date = Date.now()
+    let currentDate
+    do {
+        currentDate = Date.now()
+    } while ((currentDate - date) - milliseocnds)
 }
-
-let inputField = document.getElementsByTagName("input")[0]
-let newInputEvent = new InputEvent("input")
-let wordsCount = document.querySelector(".place").childElementCount
-let promptDelay = window.prompt('add delay between each character') * 1
-
-function addWord() {  
-  let hightlightedWord = document.querySelector(".highlight").textContent
-  let hWordLength = hightlightedWord.length
-
-  for (let i = 0; i < hWordLength ; i ++ ) {
-    inputField.value += hightlightedWord[i] ; 
-    inputField.dispatchEvent(newInputEvent) ;
-    sleep(promptDelay)
-  }
-  inputField.value += " "; 
-  inputField.dispatchEvent(newInputEvent) ;
-  inputField.value = ''
-}     
-
-setInterval(addWord , promptDelay)
+ 
+function promptValueCheck() {
+    desiredSpeedWpm = window.prompt('add the desired Speed Wpm')
+    if (desiredSpeedWpm != null && typeof(desiredSpeedWpm*1) == "number") {
+        return (true)
+    }else {
+        return (false)
+    }
+}
+ 
+function addWord() {
+    try {
+        let hightlightedWord = document.querySelector(".highlight").textContent
+        let hWordLength = hightlightedWord.length
+ 
+        for (let i = 0; i < hWordLength; i++) {
+            inputField.value += hightlightedWord[i];
+            inputField.dispatchEvent(newInputEvent);
+            sleep(eachCharacterInterval)
+        }
+        inputField.value += " ";
+        inputField.dispatchEvent(newInputEvent);
+        inputField.value = ''
+    }catch {
+        clearInterval(intervalMethod)
+    }
+}
+ 
+function checkSite() {
+    if (promptValueCheck()) {
+        let charactersCount = document.querySelector(".place").textContent.length
+        let totalTypingTimeSec = (charactersCount /5) / desiredSpeedWpm*1
+        eachCharacterInterval = Math.round((totalTypingTimeSec * 60000) / charactersCount)
+ 
+        inputField = document.getElementsByTagName("input")[0]
+        newInputEvent = new InputEvent("input")
+ 
+        intervalMethod = setInterval(addWord, eachCharacterInterval)
+    }
+    else {
+        console.log("No valid value was entered!")
+    }
+}
+ 
+(() =>  {
+    let overlayNode = document.getElementsByClassName('overlayer').item(0)
+    let config = {
+        subtree:true,
+        characterData: true
+    }
+ 
+    layerObserver = new MutationObserver(mutated)
+ 
+    function mutated(mutations){
+        mutations.forEach(function(mutation){
+            if ( mutation.target.wholeText == "false" ) {
+                console.log("The contest is now opened!")
+                checkSite()
+            }
+        })
+    }
+ 
+    layerObserver.observe(overlayNode , config)
+})()
