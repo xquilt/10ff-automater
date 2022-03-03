caac7ec98abdff09c68444efc40a74a319a83dc4
z0xyz <solicitmind@gmail.com>
2022-03-03 15:30:03 +0200

    Testing if i can commit

--- main.js
+++ main.js
@@ -4,22 +4,23 @@ function sleep(milliseocnds) {
     do {
         currentDate = Date.now()
     } while ((currentDate - date) - milliseocnds)
+
 }
- 
+
 function promptValueCheck() {
     desiredSpeedWpm = window.prompt('add the desired Speed Wpm')
-    if (desiredSpeedWpm != null && typeof(desiredSpeedWpm*1) == "number") {
+    if (desiredSpeedWpm != null && typeof (desiredSpeedWpm * 1) == "number") {
         return (true)
-    }else {
+    } else {
         return (false)
     }
 }
- 
+
 function addWord() {
     try {
         let hightlightedWord = document.querySelector(".highlight").textContent
         let hWordLength = hightlightedWord.length
- 
+
         for (let i = 0; i < hWordLength; i++) {
             inputField.value += hightlightedWord[i];
             inputField.dispatchEvent(newInputEvent);
@@ -28,44 +29,44 @@ function addWord() {
         inputField.value += " ";
         inputField.dispatchEvent(newInputEvent);
         inputField.value = ''
-    }catch {
+    } catch {
         clearInterval(intervalMethod)
     }
 }
- 
+
 function checkSite() {
     if (promptValueCheck()) {
         let charactersCount = document.querySelector(".place").textContent.length
-        let totalTypingTimeSec = (charactersCount /5) / desiredSpeedWpm*1
+        let totalTypingTimeSec = (charactersCount / 5) / desiredSpeedWpm * 1
         eachCharacterInterval = Math.round((totalTypingTimeSec * 60000) / charactersCount)
- 
+
         inputField = document.getElementsByTagName("input")[0]
         newInputEvent = new InputEvent("input")
- 
+
         intervalMethod = setInterval(addWord, eachCharacterInterval)
     }
     else {
         console.log("No valid value was entered!")
     }
 }
- 
-(() =>  {
+
+(() => {
     let overlayNode = document.getElementsByClassName('overlayer').item(0)
     let config = {
-        subtree:true,
+        subtree: true,
         characterData: true
     }
- 
+
     layerObserver = new MutationObserver(mutated)
- 
-    function mutated(mutations){
-        mutations.forEach(function(mutation){
-            if ( mutation.target.wholeText == "false" ) {
+
+    function mutated(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.target.wholeText == "false") {
                 console.log("The contest is now opened!")
                 checkSite()
             }
         })
     }
- 
-    layerObserver.observe(overlayNode , config)
+
+    layerObserver.observe(overlayNode, config)
 })()
