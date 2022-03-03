3fe23ca40d0945139eac1b008ff8264cf4a42434
z0xyz <solicitmind@gmail.com>
2022-03-03 15:34:23 +0200

    New radical changes:Renamed the file main.js to user-script.js && Appended user-script metadata accordingly

--- user-script.js
+++ user-script.js
@@ -1,20 +1,51 @@
+// ==UserScript==
+// @name         10ff speed hack
+// @namespace    http://tampermonkey.net/
+// @version      0.5
+// @description  JS script to automate the input of characters at 10ff.net
+// @author       z0xyz
+// @match        https://10ff.net/*
+// @run-at       document
+// ==/UserScript==
+
 function sleep(milliseocnds) {
     const date = Date.now()
     let currentDate
     do {
         currentDate = Date.now()
     } while ((currentDate - date) - milliseocnds)
-
 }
 
+/*
+- User input's data type validation
+- This fucntion should be in a more compact form,
+and the variable should be returned somehow to be used somewhere else.
+*/
 function promptValueCheck() {
-    desiredSpeedWpm = window.prompt('add the desired Speed Wpm')
-    if (desiredSpeedWpm != null && typeof (desiredSpeedWpm * 1) == "number") {
+    let desiredWpmSpeed = window.prompt('Add the desired Speed (WPM)!')
+    if (Number.isInteger(parseInt(desiredWpmSpeed))) {
         return (true)
     } else {
         return (false)
     }
 }
+
+console.log(Number.isInteger(someValue))
+
+let someValue = '20'
+console.log(parseInt('10'))
+console.log(parseInt('10.02'))
+console.log(parseInt('10.33'))
+console.log(parseFloat('10.33'))
+console.log(Number('10.33'))
+console.log(parseInt('10 20 30'))
+console.log(parseInt(' 10 '))
+console.log(Number.isInteger(parseInt(' 10 ')))
+console.log(parseInt('He was 10'))
+console.log(parseInt('10 He was '))
+console.log(Number('10 He was '))
+console.log(Number('He was 10'))
+
 
 function addWord() {
     try {
@@ -37,7 +68,7 @@ function addWord() {
 function checkSite() {
     if (promptValueCheck()) {
         let charactersCount = document.querySelector(".place").textContent.length
-        let totalTypingTimeSec = (charactersCount / 5) / desiredSpeedWpm * 1
+        let totalTypingTimeSec = (charactersCount / 5) / desiredWpmSpeed * 1
         eachCharacterInterval = Math.round((totalTypingTimeSec * 60000) / charactersCount)
 
         inputField = document.getElementsByTagName("input")[0]
@@ -70,3 +101,8 @@ function checkSite() {
 
     layerObserver.observe(overlayNode, config)
 })()
+
+//Add the functionality to insert arbitrary randomized characters ; to acquire credibility
+//Alter the userscript description with things like "approximate"
+
+//We can randomize the typingTime variable in a range between
